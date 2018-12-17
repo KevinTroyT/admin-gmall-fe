0d6c24144c73326a70c8110343268b52965e9ac9
KevinTroyT <q964049459@gmail.com>
2018-12-17 16:20:42 +0800

    init

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+/**
+ * @Author: troykevin
+ * @Date:   2018-12-17T11:45:32+08:00
+ * @Email:  q964049459@gmail.com
+ * @Last modified by:   troykevin
+ * @Last modified time: 2018-12-17T15:38:54+08:00
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+
+ReactDOM.render(
+    <h1>Hello,Wo22rld!</h1>,
+    document.getElementById('app')
+);
