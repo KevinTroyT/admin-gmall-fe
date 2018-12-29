305f4b3e886aa95e6ff92ac43e391447f2fd1c2a
KevinTroyT <q964049459@gmail.com>
2018-12-29 14:53:37 +0800

    bug-fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
  * @Date:   2018-12-17T11:43:04+08:00
  * @Email:  q964049459@gmail.com
  * @Last modified by:   troykevin
- * @Last modified time: 2018-12-29T13:28:04+08:00
+ * @Last modified time: 2018-12-29T14:52:36+08:00
  */
 const ExtractTextPlugin     = require('extract-text-webpack-plugin');
 const webpack               = require('webpack');
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path : path.resolve(__dirname,'dist'),
         publicPath : WEBPACK_ENV === 'dev'
-            ? '/dist/' : '//admin.gitmall.cn',
+            ? '/dist/' : '//s.gitmall.cn/admin-gmall-fe/dist/',
         filename : 'js/app.js'
     },
     resolve:{
